c2baa430838ea4c33726c6b17a38d88a90528e2b
Bilal Akram <work.bilalakram@gmail.com>
2025-09-11 16:35:31 +0200

    Initial Commit

---
+++ IT Today/IT_TodayApp.swift
@@ -0,0 +1,17 @@
+//
+//  IT_TodayApp.swift
+//  IT Today
+//
+//  Created by Bilal Akram on 9/11/25.
+//
+
+import SwiftUI
+
+@main
+struct IT_TodayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
